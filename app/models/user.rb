0056db5dbbbb0671eda8bbc81fddb561a61f2c62
erib <eri-b@users.noreply.github.com>
2019-07-15 10:47:26 -0400

    index view, need to setup associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  # validates :name,  presence: true, length: { maximum: 50 }
-
   has_many :events
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index]
 end
