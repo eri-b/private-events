36fd6d972c5d8e40d60ae174450f613d36abc20f
DevMFernandes <contato@marcos-fernandes.com>
2019-07-15 17:21:16 -0300

    posting events giving error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,30 @@
 class EventsController < ApplicationController
+
+  before_action :logged_in_user, only: [:new, :create]
+
   def new
     @event = Event.new
   end
 
   def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to events_path, notice: 'Events was successfully created.'
+    else
+      #flash.now[:danger] = 'Post not saved.'
+      render 'new'
+    end
   end
 
   def show
   end
 
   def index
+    @events = Event.all
+  end
+
+  def event_params
+    params.require(:event).permit(:description)
   end
 end
