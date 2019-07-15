f835df87b35f9cd9a7892a617e78bf40a77413df
DevMFernandes <contato@marcos-fernandes.com>
2019-07-15 11:12:49 -0300

    got started on basic events, done 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   # validates :name,  presence: true, length: { maximum: 50 }
+
+  has_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_193139) do
+ActiveRecord::Schema.define(version: 2019_07_15_140623) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
