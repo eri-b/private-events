a4a41cd1ce81760e837c1901413566ee6fe63098
erib <eri-b@users.noreply.github.com>
2019-07-15 09:34:27 -0400

    persistent user name at top, end of setup, need to add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(id: params[:session][:user_id])
+    if user
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid user id'
+      render 'new'
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # root 'sessions#new'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+
   resources :users, only: [:new, :create, :show]
 end
