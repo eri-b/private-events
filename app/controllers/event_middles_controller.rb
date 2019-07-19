d81730e72840e0cd8fcdd6a389dcd602a02a2c40
erib <eri-b@users.noreply.github.com>
2019-07-18 20:12:01 -0400

    small update

--- app/controllers/event_middles_controller.rb
+++ app/controllers/event_middles_controller.rb
@@ -1,7 +1,9 @@
 class EventMiddlesController < ApplicationController
   def create
 	  @event = Event.find(params[:event_middle][:attended_event_id])
+
     current_user.event_middles.create(attended_event_id: @event.id)
 	  redirect_to @event
   end
+
 end
